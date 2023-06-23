16e28e1552ee4b2235b6bc368a752ed6ae0ccfb9
Nemuel Wainaina <bR0S3@protonmail.com>
2023-06-23 13:40:49 +0300

    Create Hero, LogIn and Register page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import NavBar from '../components/landing/NavBar'
-import Hero from '../components/landing/Hero'
+// import Hero from '../components/landing/Hero'
+// import LogIn from '../components/customer/LogIn'
+import Register from '../components/customer/Register'
 
 const Landing = () => {
   return (
     <>
         <NavBar />
-        <div style={{height: '89vh'}} className="d-flex align-items-center justify-content-center">
-            <Hero />
-        </div>
+        <Register />
     </>
   )
 }
