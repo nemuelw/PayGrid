785ab9ca323c546f6317e507fbdd2ebf5144170d
Nemuel Wainaina <bR0S3@protonmail.com>
2023-07-03 08:31:59 +0300

    Fix Nav bugs and optimize performance

--- frontend/src/components/landing/NavBar.js
+++ frontend/src/components/landing/NavBar.js
@@ -2,17 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+  const navigate = useNavigate()
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary" style={{height: '10vh'}}>
       <Container>
-        <Navbar.Brand href="/">PayGrid</Navbar.Brand>
+        <Navbar.Brand style={{cursor:'pointer'}} onClick={() => navigate('/')}>PayGrid</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav className="d-flex align-items-center">
-            <Nav.Link href="/login">Log In</Nav.Link>
-            <Nav.Link href="/register">
+            <Nav.Link onClick={() => navigate('/login')}>Log In</Nav.Link>
+            <Nav.Link onClick={() => navigate('/register')}>
                 <Button variant="outline-primary">Register</Button>
             </Nav.Link>
           </Nav>
