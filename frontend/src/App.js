1777547ad7186c49f4c28760f25d75fa9fb68b1a
Nemuel Wainaina <bR0S3@protonmail.com>
2023-07-03 21:31:32 +0300

    Add Admin login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import ALogIn from "./pages/ALogIn";
 import Landing from "./pages/Landing";
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={ <Landing/> } />
         <Route path="login" element={ <LogIn/> } />
         <Route path="register" element={ <Register/> } />
+        <Route path="admin" element={ <ALogIn/> } />
       </Routes>
     </div>
   );
--- frontend/src/pages/ALogIn.jsx
+++ frontend/src/pages/ALogIn.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Card, Form, Row, Col, Button } from 'react-bootstrap'
 
 const ALogIn = () => {
   return (
-    <div>
-      
+    <div style={{height: '90vh'}} className="d-flex align-items-center justify-content-center">
+        <Card className="rounded mx-auto mt-5">
+            <Card.Body className="text-center">
+                <Card.Title>Admin Log In</Card.Title>
+                <hr />
+                <Form className="px-4 mx-2">
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Control required type="email" placeholder="Email Address" />
+                </Form.Group>
+                <Form.Group controlId="password">
+                    <Form.Control required type="password" placeholder="Password" />
+                </Form.Group>
+                <Row className="mt-4 d-flex align-items-center">
+                    <Col xs={6} className="text-start">
+                    <a href='/forgot' style={{fontSize: '97%'}} className="text-decoration-none">
+                        Forgot password?
+                    </a>
+                    </Col>
+                    <Col xs={6} className="text-end">
+                    <Button variant="primary" type="submit">
+                        Log In
+                    </Button>
+                    </Col>
+                </Row>
+                </Form>
+            </Card.Body>
+        </Card>
     </div>
   )
 }
