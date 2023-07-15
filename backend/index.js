7e5d4c304b50fc542e50dfe1525b0cef43e14094
Nemuel Wainaina <bR0S3@protonmail.com>
2023-07-15 17:49:27 +0300

    Define the main server script

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log('Welcome to the backend of the system')
+const express = require('express')
+const cors = require('cors')
+require('./database')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const accountsRouter = require('./routes/accounts')
+const authRouter = require('./routes/auth')
+const clearedRouter = require('./routes/cleared')
+const paymentRouter = require('./routes/payment')
+const pendingRouter = require('./routes/pending')
+
+app.use('/accounts', accountsRouter)
+app.use('/auth', authRouter)
+app.use('/cleared', clearedRouter)
+app.use('/payment', paymentRouter)
+app.use('/pending', pendingRouter)
+
+app.listen(8000, () => {
+    console.log('Server running ...')
+})
