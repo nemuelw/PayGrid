12c47ba7a47936e8ae944454178fdaf237c94b80
Nemuel Wainaina <bR0S3@protonmail.com>
2023-07-20 08:12:34 +0300

    Initialize portal

--- frontend/src/pages/Portal.jsx
+++ frontend/src/pages/Portal.jsx
@@ -1,7 +1,35 @@
+import { useEffect,useState } from 'react';
+import axios from 'axios'
 import { Container } from 'react-bootstrap';
 import NavBar from '../components/customer/NavBar';
 
 const Portal = () => {
+  const [cleared, setCleared] = useState([])
+  const [pending, setPending] = useState([])
+
+  useEffect(() => {
+    fetchClearedBills();
+    fetchPendingBills();
+  }, [])
+
+  const fetchClearedBills = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/cleared')
+      setCleared(response.data)
+    } catch (err) {
+      console.log('Error occurred : ' + err)
+    }
+  }
+
+  const fetchPendingBills = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/pending')
+      setPending(response.data)
+    } catch (err) {
+      console.log('Error occurred : ' + err)
+    }
+  }
+
   return (
     <>
         <NavBar />
