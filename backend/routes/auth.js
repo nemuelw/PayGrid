745010186b3a636ca5eb81ce33125dcb8969459d
Nemuel Wainaina <bR0S3@protonmail.com>
2023-07-29 00:00:00 +0300

    Integrate jwt tokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,18 @@
 // handles requests regarding user authentication (customer and admin)
 
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 
 const Account = require('../models/Account')
+const { TokenExpiredError } = require('jsonwebtoken')
 
 // admin login
 router.post('/a', (req, res) => {
     const {email, passwd} = req.body
     if (email === 'admin@paygrid.com' && passwd === 'password123') {
-        
-        res.json({msg: 'success'})
+        token = jwt.sign({email}, process.env.ADMIN_TOKEN)
+        res.json({msg: 'success', token: token})
     } else {
         res.json({msg: 'invalid_creds'})
     }
@@ -23,7 +25,8 @@ router.post('/c', async (req, res) => {
         const account = await Account.findOne({email_address: email})
         if (account) {
             if (passwd === account.passwd_hash) {
-                res.json({msg: 'success'})
+                token = jwt.sign({email}, process.env.CUSTOMER_TOKEN)
+                res.json({msg: 'success', token: token})
             } else {
                 res.json({msg: 'invalid_creds'})
             }
